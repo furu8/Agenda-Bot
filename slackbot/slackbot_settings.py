af303b099b0909057110929c0a0dd9d00789600f
furu8 <foooolurk1125@gmail.com>
2019-09-07 17:17:51 +0900

    first commit

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,15 @@
+# テキストファイル読み込み
+f = open('../token.txt', 'r')
+token = f.read()
+# print(token)
+
+# トークン
+API_TOKEN = token
+
+# 応答サンプル
+DEFAULT_REPLY = 'は？'
+
+# プラグイン
+PULUGINS = ['plugins']
+
+f.close()
